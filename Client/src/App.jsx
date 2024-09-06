24a284397d802ce540dc2dd311676d0f529e7820
ryomensukuna2003 <mshivanshu1264@gmail.com>
2024-09-06 21:31:52 +0530

    Added Shadcn

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,12 @@
+import {Button} from './components/ui/button'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+    <div className='grid justify-items-center w-screen'>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+      <Button>Click meeeee</Button>
+    </div>
   )
 }
